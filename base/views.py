79e6aecf8dda52d0d4fd7db22a17ded329bdbc4d
Subash Pariyar <subashpariyar61@gmail.com>
2024-09-11 15:47:05 +0545

    commit changes

--- base/views.py
+++ base/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    todo_objs = Todo.objects.all()
+    data = {'todos'  : todo_objs}
+
+    return render(request, 'index.html',context=data)
 
